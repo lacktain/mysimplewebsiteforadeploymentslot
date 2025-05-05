442caa005767a437d490520a406f1353d53805b1
lacktain <59469987+lacktain@users.noreply.github.com>
2025-05-05 18:59:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Hello from GitHub to Azure!</h1>');
+  res.end('<h1>Hello from GitHub to Azure using Node!</h1>');
 }).listen(port);
