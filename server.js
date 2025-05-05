c97d02edb2ba3e4c6639e3d91ee8e6b0ddb3c9ea
lacktain <59469987+lacktain@users.noreply.github.com>
2025-05-05 18:34:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const http = require('http');
 
+const port = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello from Node.js on Azure App Service!</h1>');
+  res.end('<h1>Hello from Node.js on Azure!</h1>');
 });
 
-const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
