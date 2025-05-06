b435fc7748f1489213ec3a584796a1266c21769a
lacktain <59469987+lacktain@users.noreply.github.com>
2025-05-06 09:47:30 +0200

    Update app.py
    writing error fixed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello from Azure App Service running python! Developers like to worki in GIT</h1>"
+    return "<h1>Hello from Azure App Service running python! Developers like to work in GIT</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
