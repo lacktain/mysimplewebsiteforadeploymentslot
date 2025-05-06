303e4270a335cd00be6540c1ce51e56dfc1e30d5
lacktain <59469987+lacktain@users.noreply.github.com>
2025-05-06 11:58:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-echo "<h1>Hello from Azure App Service running PHP! The coder like this very much. It just works. </h1>";
+echo "<h1>Hello from Azure App Service running PHP! The coder like this very much. It just works. Very vell</h1>";
 ?>
